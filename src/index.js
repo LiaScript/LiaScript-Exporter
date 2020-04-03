721707046310b7da99b159554fca979b69f405e3
André Dietrich <andre.dietrich@ovgu.de>
2020-04-03 21:42:51 +0200

    scorm builder seem to work so far ...

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import { Elm } from '../LiaScript/src/elm/Worker.elm'
 
-const fs = require('fs');
+var scopackager = require('simple-scorm-packager');
+var path = require('path');
+
+var fs = require('fs-extra');
 var argv = require('minimist')(process.argv.slice(2));
 
+console.warn(argv);
 
 function help() {
   console.log("LiaScript-Exporter")
   console.log("")
   console.log("-h", "--help", "           show this help")
   console.log("-i", "--input", "          file to be used as input")
+  console.log("-p", "--path", "           path to be packed, if not set, the path of the input file is used")
   console.log("-o", "--output", "         output file name (default is output), the ending is define by the format")
   console.log("-f", "--format", "         scorm1.2, json, web (default is json)")
-  console.log("-b", "--base", "           define a base-path, default is './' ")
-
+  //console.log("-b", "--base", "           define a base-path, default is './' ")
+  console.log("-k", "--key", "            responsive voice key ")
 }
 
 
@@ -41,12 +46,88 @@ if (argv.h || argv.help) {
         })
         break
       }
+      case "scorm1.2": {
+        let json = JSON.parse(string)
+
+        let readme = argv.i || argv.input
+
+        const config = {
+          version: '1.2',
+          organization: 'LiaScript',
+          title: json.str_title,
+          language: json.definition.language,
+          //masteryScore: 80,
+          startingPage: 'index.html',
+          startingParameters: path.basename(readme),
+          source: path.join(__dirname, '../build'),
+          package: {
+            version: json.definition.version,
+            zip: true,
+            name: output,
+            author: json.definition.author,
+            outputFolder: path.join(__dirname, 'scorm_packages'),
+            description: json.comment,
+            //keywords: ['scorm', 'test', 'course'],
+            //typicalDuration: 'PT0H5M0S',
+            //rights: `©${new Date().getFullYear()} My Amazing Company. All right reserved.`,
+            vcard: {
+              author: json.definition.author,
+              //org: 'My Amazing Company',
+              //tel: '(000) 000-0000',
+              //address: 'my address',
+              mail: json.definition.email
+              //url: 'https://mydomain.com'
+            }
+          }
+        };
+
+        fs.copy(
+            path.join(__dirname, '../assets/scorm1.2'),
+            path.join(__dirname, '../build')
+          )
+          .then(() => {
+            let key = argv.k || argv.key
+            if (key){
+              try {
+                let index = fs.readFileSync(path.join(__dirname, '../build/index.html'), 'utf8')
+
+                index = index.replace(
+                  "https://code.responsivevoice.org/responsivevoice.js",
+                  "https://code.responsivevoice.org/responsivevoice.js?key="+key
+                )
+
+                fs.writeFile(path.join(__dirname, '../build/index.html'), index, function (err) {
+                  if (err) {
+                    console.error(err)
+                    return
+                  }
+
+
+                  fs.copy(
+                    path.dirname(readme),
+                    path.join(__dirname, '../build')
+                  )
+                  .then(() => {
+                    scopackager(config, function(msg){
+                      console.log(msg);
+                      process.exit(0);
+                    });
+                  })
+                })
+              } catch (err) {
+                console.error(err)
+              }
+            }
+          })
+          .catch(err => console.error(err))
+
+
+        break;
+      }
       default: {
         console.warn("unknown output format", format);
       }
     }
-
-
   })
 
   try {
