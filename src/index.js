f793278455c6fa75fae029b4acfa4f007bbbd895
André Dietrich <andre.dietrich@ovgu.de>
2020-04-04 16:10:41 +0200

    temporary files work

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { Elm } from '../LiaScript/src/elm/Worker.elm'
 
-var scopackager = require('simple-scorm-packager');
-var path = require('path');
-
-var fs = require('fs-extra');
-var argv = require('minimist')(process.argv.slice(2));
 global.XMLHttpRequest = require('xhr2');
+
+var scopackager = require('simple-scorm-packager'),
+    path = require('path'),
+    temp = require('temp'),
+    fs   = require('fs-extra'),
+    argv = require('minimist')(process.argv.slice(2));
+
 
 //console.warn(argv);
 
@@ -52,76 +54,81 @@ if (argv.h || argv.help) {
 
         let readme = argv.i || argv.input
 
-        const config = {
-          version: '1.2',
-          organization: 'LiaScript',
-          title: json.str_title,
-          language: json.definition.language,
-          //masteryScore: 80,
-          startingPage: 'index.html',
-          startingParameters: path.basename(readme),
-          source: path.join(__dirname, '../build'),
-          package: {
-            version: json.definition.version,
-            zip: true,
-            name: output,
-            author: json.definition.author,
-            outputFolder: path.join(__dirname, 'scorm_packages'),
-            description: json.comment,
-            //keywords: ['scorm', 'test', 'course'],
-            //typicalDuration: 'PT0H5M0S',
-            //rights: `©${new Date().getFullYear()} My Amazing Company. All right reserved.`,
-            vcard: {
+        temp.mkdir('scorm', function(err, tmp) {
+
+          console.warn(tmp);
+
+          let tempPath = path.join(tmp, "pro")
+
+          const config = {
+            version: '1.2',
+            organization: 'LiaScript',
+            title: json.str_title,
+            language: json.definition.language,
+            //masteryScore: 80,
+            startingPage: 'index.html',
+            startingParameters: path.basename(readme),
+            source: tempPath,
+            package: {
+              version: json.definition.version,
+              zip: true,
+              name: output,
               author: json.definition.author,
-              //org: 'My Amazing Company',
-              //tel: '(000) 000-0000',
-              //address: 'my address',
-              mail: json.definition.email
-              //url: 'https://mydomain.com'
+              outputFolder: path.join(tmp, 'scorm_packages'),
+              description: json.comment,
+              //keywords: ['scorm', 'test', 'course'],
+              //typicalDuration: 'PT0H5M0S',
+              //rights: `©${new Date().getFullYear()} My Amazing Company. All right reserved.`,
+              vcard: {
+                author: json.definition.author,
+                //org: 'My Amazing Company',
+                //tel: '(000) 000-0000',
+                //address: 'my address',
+                mail: json.definition.email
+                //url: 'https://mydomain.com'
+              }
             }
-          }
-        };
+          };
 
-        fs.copy(
-            path.join(__dirname, './assets/scorm1.2'),
-            path.join(__dirname, '../build')
-          )
-          .then(() => {
-            let key = argv.k || argv.key
-            if (key){
-              try {
-                let index = fs.readFileSync(path.join(__dirname, '../build/index.html'), 'utf8')
 
+          fs.copy(
+              path.join(__dirname, './assets/scorm1.2'),
+              tempPath
+            )
+            .then(() => {
+              console.warn("copy assets ... done");
+
+              let key = argv.k || argv.key
+
+              let index = fs.readFileSync(path.join(tempPath, 'index.html'), 'utf8')
+
+              if (key) {
                 index = index.replace(
                   "https://code.responsivevoice.org/responsivevoice.js",
                   "https://code.responsivevoice.org/responsivevoice.js?key="+key
-                )
-
-                fs.writeFile(path.join(__dirname, '../build/index.html'), index, function (err) {
-                  if (err) {
-                    console.error(err)
-                    return
-                  }
-
-
-                  fs.copy(
-                    path.dirname(readme),
-                    path.join(__dirname, '../build')
                   )
-                  .then(() => {
-                    scopackager(config, function(msg){
-                      console.log(msg);
-                      process.exit(0);
-                    });
-                  })
-                })
-              } catch (err) {
-                console.error(err)
               }
-            }
-          })
-          .catch(err => console.error(err))
 
+              fs.writeFile(path.join(tempPath, 'index.html'), index, function (err) {
+                if (err) {
+                  console.error(err)
+                  return
+                }
+
+                fs.copy(
+                  path.dirname(readme),
+                  tempPath
+                )
+                .then(() => {
+                  scopackager(config, function(msg){
+                    console.log(msg);
+                    process.exit(0);
+                  });
+                })
+              })
+            })
+          })
+        
 
         break;
       }
