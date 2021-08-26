c331344e060d418de8175a005667504dcef9ee12
André Dietrich <andredietrich@web.de>
2021-08-26 21:51:49 +0200

    add web project output

--- src/index.ts
+++ src/index.ts
@@ -175,17 +175,54 @@ async function web(argv, json) {
   let key = argv.k || argv.key
   if (key) {
     index = index.replace(
-      'https://code.responsivevoice.org/responsivevoice.js',
-      'https://code.responsivevoice.org/responsivevoice.js?key=' + key
+      '</head>',
+      `<script src="https://code.responsivevoice.org/responsivevoice.js?key=${key}"></script></head>`
     )
   }
 
+  // add default course
   index = index.replace(
     '<head>',
     '<head><script>window.liaDefaultCourse="' +
       path.basename(readme) +
       '"</script>'
   )
+
+  try {
+    index = index.replace(
+      '<title>Lia</title>',
+      `<title>${json.lia.str_title}</title><meta property="og:title" content="${json.lia.str_title}"> <meta name="twitter:title" content="${json.lia.str_title}">`
+    )
+    console.log('updating title ...')
+  } catch (e) {
+    console.warn('could not add title')
+  }
+
+  // add description
+  try {
+    let description = json.lia.definition.macro.comment
+
+    index = index.replace(
+      '<meta name="description" content="LiaScript is a service for running free and interactive online courses, build with its own Markup-language. So check out the following course ;-)">',
+      `<meta name="description" content="${description}"><meta property="og:description" content="${description}"><meta name="twitter:description" content="${description}">`
+    )
+
+    console.log('updating description ...')
+  } catch (e) {
+    console.warn('could not add description')
+  }
+
+  try {
+    let logo = json.lia.definition.logo
+    index = index.replace(
+      '<head>',
+      `<head><meta property="og:image" content="${logo}"><meta name="twitter:image" content="${logo}">`
+    )
+
+    console.log('updating logo ...')
+  } catch (e) {
+    console.warn('could not add image')
+  }
 
   try {
     await writeFile(path.join(tmpPath, 'index.html'), index)
