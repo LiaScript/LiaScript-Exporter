c7162e6d6dbd31baa1ba5f9c34ba4cf338393dfb
André Dietrich <andredietrich@web.de>
2021-10-28 21:11:01 +0200

    Version bump to 1.0.46

--- src/index.ts
+++ src/index.ts
@@ -237,7 +237,7 @@ async function web(argv, json) {
 }
 
 if (argv.v || argv.version) {
-  console.log('version: 1.0.45--0.9.40')
+  console.log('version: 1.0.46--0.9.40')
 } else if (argv.h || argv.help) {
   help()
 } else if (argv.i || argv.input) {
