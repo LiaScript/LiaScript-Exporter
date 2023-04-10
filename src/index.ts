eb03259e2b4ef295d6a2835887bf640c6dd1f23c
André Dietrich <andredietrich@web.de>
2023-04-10 21:41:58 +0200

    Upgrade to LiaScript 0.11.1

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ import fetch from 'node-fetch'
 
 // -------------------------------Main Execution-------------------------------
 if (argv.v || argv.version) {
-  console.log('version: 2.6.7--0.11.0')
+  console.log('version: 2.6.7--0.11.1')
 } else if (argv.h || argv.help) {
   help()
 } else if (argv.i || argv.input) {
