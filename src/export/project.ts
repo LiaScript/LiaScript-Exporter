086986396d325f34645d244f338a74a6c6450325
André Dietrich <andredietrich@web.de>
2023-06-06 12:22:08 +0200

    fix: Metadata generation

--- src/export/project.ts
+++ src/export/project.ts
@@ -243,7 +243,7 @@ export async function exporter(
         : ''
     }
 
-    ${argument['project-no-meta'] ? '' : meta(json)}
+    ${!argument['project-no-meta'] ? '' : meta(json)}
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
