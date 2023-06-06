f5e469ef13e9e33dfdf34e738c7e7ad1e2e9a0a4
André Dietrich <andredietrich@web.de>
2023-06-06 12:23:05 +0200

    Version bump to 2.6.17

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ import fetch from 'node-fetch'
 
 // -------------------------------Main Execution-------------------------------
 if (argv.v || argv.version) {
-  console.log('version: 2.6.16--0.12.2')
+  console.log('version: 2.6.17--0.12.2')
 } else if (argv.h || argv.help) {
   help()
 } else if (argv.i || argv.input) {
