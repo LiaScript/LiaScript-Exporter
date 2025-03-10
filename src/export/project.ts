fb49e261b381df3498f62affb01468c1d7f38062
André Dietrich <andredietrich@web.de>
2025-03-10 09:11:00 +0100

    add viewport to project export

--- src/export/project.ts
+++ src/export/project.ts
@@ -246,6 +246,7 @@ export async function exporter(
         : ''
     }
 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <script>
