45369a54f32dfbdea26ab3388812f8bd54ae914d
André Dietrich <andredietrich@web.de>
2026-03-05 09:19:01 +0100

    Update LiaScript build

--- dist/assets/web/snippets/clue.js
+++ dist/assets/web/snippets/clue.js
@@ -1,1 +1,8 @@
-ace.require(["ace/snippets/clue"],(function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)}));
+;                (function() {
+                    ace.require(["ace/snippets/clue"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
